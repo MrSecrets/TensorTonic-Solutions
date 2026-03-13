1034ba478e702bc6d7b298716beb56f0de91e544
Sreeraj S J <31489737+MrSecrets@users.noreply.github.com>
2026-03-13 15:27:37 +0530

    Add solution for transformers-attention

---
+++ transformer/transformers-attention/transformers-attention.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn.functional as F
+import math
+
+def scaled_dot_product_attention(Q: torch.Tensor, K: torch.Tensor, V: torch.Tensor) -> torch.Tensor:
+    """
+    Compute scaled dot-product attention.
+    """
+    # Your code here
+    d_k = K.size(-1)
+    scale = 1 / math.sqrt(d_k)
+
+    inner = (Q @ K.transpose(-2, -1)) * scale
+    soft_inner = torch.softmax(inner, dim=-1)
+    attention = soft_inner @ V
+
+    return attention
