3ce331c9249827ba98507046c061558c05985873
Sreeraj S J <31489737+MrSecrets@users.noreply.github.com>
2026-06-08 22:58:38 +0530

    Solve nesterov-momentum

---
+++ nesterov-momentum/nesterov-momentum.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def nesterov_momentum_step(w, v, grad, lr=0.01, momentum=0.9):
+    """
+    Perform one Nesterov Momentum update step.
+    """
+    # Write code here
+
+    # w_look = w - momentum*v
+    # g_look = grad[w_look] already computed in grad
+    w, v, grad = np.asarray(w), np.asarray(v), np.asarray(grad)
+    v_new = momentum*v + lr*grad
+    w_new = w-v_new
+
+    return (w_new, v_new)
+    
