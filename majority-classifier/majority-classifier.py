4a884a584839ae6b442c54dbfb7da2858a393c99
Sreeraj S J <31489737+MrSecrets@users.noreply.github.com>
2026-06-20 19:35:25 +0530

    Solve majority-classifier

--- majority-classifier/majority-classifier.py
+++ majority-classifier/majority-classifier.py
@@ -7,9 +7,9 @@ def majority_classifier(y_train, X_test):
     # Write code here
     y_train, X_test = np.asarray(y_train), np.asarray(X_test)
     labels,counts = np.unique(y_train, return_counts=True)
-    print(labels)
+    # print(labels)
     largest = labels[np.argmax(counts)]
     size_ = X_test.shape
 
-    print(largest, size_)
+    # print(largest, size_)
     return np.full(size_, fill_value=largest)
