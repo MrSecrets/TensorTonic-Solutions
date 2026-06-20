e5c831b62502c89ba0a484c4cdf7a2b700ffca4c
Sreeraj S J <31489737+MrSecrets@users.noreply.github.com>
2026-06-20 23:38:39 +0530

    Solve rank-transform

---
+++ rank-transform/rank-transform.py
@@ -0,0 +1,25 @@
+def rank_transform(values):
+    """
+    Replace each value with its average rank.
+    """
+    counter = {}
+    for v in values:
+        curr = counter.get(v, 0)
+        counter[v] = curr+1
+
+    counter_list = []
+    for key, value in counter.items():
+        elem = (key, value)
+        counter_list.append(elem)
+    counter_list.sort()
+
+    i=0
+    for (key, value) in counter_list:
+        rank = ((i+1) + (i+value))/2            
+        counter[key] = rank
+        i+=value
+
+    encoded = []
+    for v in values:
+        encoded.append(counter[v])
+    return encoded
